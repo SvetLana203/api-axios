ce27eeb5d6bf1be67a8eff77d0ad991b18eead1c
SvetLana203 <shirapovauu@gmail.com>
2023-02-09 22:13:33 -0600

    axios

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import './App.css';
+import { Component } from "react";
+
+class App extends Component {
+state = {
+  posts: []
+}
+ 
+componentDidMount() {
+  axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+    const posts = res.data;
+    this.setState({posts});
+  })
+}
+
+render() {
+  return (
+    <div>
+      {this.state.posts.map(post => 
+        <p key={post.id}>{post.title}
+        <span> {post.body}</span>
+        </p>
+        )}
+    </div>
+    );
+  }
+}
+
+export default App;
